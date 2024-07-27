67f47e1d016701e82e66bf8d20c6a45f4620c715
Janderson <janderson@quimbik.com>
2024-07-27 15:27:56 -0300

    updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_204845) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_161826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brazilian_cup_champions", force: :cascade do |t|
+    t.string "champion_key"
+    t.string "vice_key"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "brazilian_serie_a_champions", force: :cascade do |t|
     t.integer "year"
